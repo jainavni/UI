b101d05b6d0d53d3c024a269a8ae3236acee677b
Avni Jain <aj.jainavni1998@gmail.com>
2017-07-06 17:42:29 +0530

    INITIAL COMMIT

--- app/src/main/java/com/example/avnijain/ui/ScrollingActivity.java
+++ app/src/main/java/com/example/avnijain/ui/ScrollingActivity.java
@@ -13,6 +13,8 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
